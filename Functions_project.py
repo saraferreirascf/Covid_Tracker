e9712e8d5c465cb34beb0bdff2ceb6ffa684b74d
carolbafonso <carolina_afonso4@hotmail.com>
2020-04-30 17:11:31 +0100

    primeira tentativa | TABD_projeto - trabalho (main) | Functions_project - ficheiro com as funcoes para nao ficar confuso

---
+++ Functions_project.py
@@ -0,0 +1,56 @@
+import random
+import sys
+
+###########
+# DISPLAY #
+###########
+def polygon_to_points(polygon_string):
+    xs, ys = [],[]
+    points = polygon_string[9:-2].split(',')
+    for point in points:
+        (x,y) = point.split()
+        xs.append(float(x))
+        ys.append(float(y))
+    return xs,ys
+
+#####################
+# GENERAL FUNCTIONS #
+#####################
+
+def get_infected(conn, s):
+    cursor_psql = conn.cursor()
+    cursor_psql.execute("select distinct taxi from tracks, cont_aad_caop2018 where concelho='"+str(s)+"' LIMIT 10")
+    taxis = cursor_psql.fetchall()
+    p = []
+    for row in taxis:
+        points = row[0].split(',')
+        p.append(points)
+    first_point = random.choice(p)
+    return first_point
+
+def linestring_to_points(line_string):
+    xs, ys = [],[]
+    points = line_string[11:-1].split(',')
+    for point in points:
+        (x,y) = point.split()
+        xs.append(float(x))
+        ys.append(float(y))
+    return xs,ys
+
+
+def get_taxis(conn):
+    cursor_psql = conn.cursor()
+    cursor_psql.execute("select taxi from tracks, cont_aad_caop2018 where concelho='PORTO'")
+    taxis = cursor_psql.fetchall()
+    return taxis
+
+def get_tracks(conn, taxi):
+    cursor_psql = conn.cursor()
+    cursor_psql.execute("select proj_track from tracks where taxi='"+str(taxi)+"';")
+    track = cursor_psql.fetchall()
+    return track
+'''
+def infected(taxi,conn):
+    cursor_psql = conn.cursor()
+    cursor_psql.execute("select taxi from tracks where 
+'''
---
+++ TABD_projeto.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import psycopg2
+import math
+from matplotlib.animation import FuncAnimation
+import datetime
+import csv
+from postgis import Polygon,MultiPolygon
+from postgis.psycopg import register
+from Functions_project import *
+import sys
+import random
+
+def animate(i):
+    ax.set_title(datetime.datetime.utcfromtimestamp(ts_i+i*10))
+    scat.set_offsets(offsets[i])
+    #scat2.set_offssets(offsets[i])
+    
+ts_i = 1570665600
+scale=1/3000000
+conn = psycopg2.connect("dbname=postgres")
+register(conn)
+xs_min, xs_max, ys_min, ys_max = -120000, 165000, -310000, 285000
+width_in_inches = (xs_max-xs_min)/0.0254*1.1
+height_in_inches = (ys_max-ys_min)/0.0254*1.1
+fig, ax = plt.subplots(figsize=(width_in_inches*scale, height_in_inches*scale))
+ax.axis('off')
+ax.set(xlim=(xs_min, xs_max), ylim=(ys_min, ys_max))
+cursor_psql = conn.cursor()
+
+#desenhar mapa#
+
+sql = "select distrito,st_union(proj_boundary) from cont_aad_caop2018 group by distrito"
+cursor_psql.execute(sql)
+results = cursor_psql.fetchall()
+xs , ys = [],[]
+for row in results:
+    geom = row[1]
+    if type(geom) is MultiPolygon:
+        for pol in geom:
+            xys = pol[0].coords
+            xs, ys = [],[]
+            for (x,y) in xys:
+                xs.append(x)
+                ys.append(y)
+            ax.plot(xs,ys,color='black',lw='0.2')
+    if type(geom) is Polygon:
+        xys = geom[0].coords
+        xs, ys = [],[]
+        for (x,y) in xys:
+            xs.append(x)
+            ys.append(y)
+        ax.plot(xs,ys,color='black',lw='0.2')
+
+# taxis #
+
+offsets = []
+with open('offsets3.csv', 'r') as csvFile:
+    reader = csv.reader(csvFile)
+    i = 0
+    for row in reader:
+        l = []
+        for j in row:
+            x,y = j.split()
+            x = float(x)
+            y= float(y)
+            l.append([x,y])
+        offsets.append(l)
+
+offsets = np.array(offsets)
+
+x,y = [],[]
+for i in offsets[0]:
+    x.append(i[0])
+    y.append(i[1])
+
+taxi_porto = get_infected(conn, 'PORTO')
+#taxi_lisboa = get_infected(conn, 'LISBOA')
+taxi_track_p = get_tracks(conn, taxi_porto[0])
+#taxi_track_l = get_tracks(conn, taxi_lisboa[0])
+
+xp, yp = [], []
+for row in taxi_track_p[0]:
+    xp.append(row[0][0])
+    yp.append(row[0][1])
+
+scat = ax.scatter(x,y,s=2,color='green')
+scat2 = ax.scatter(xp,yp,s=2,color='red')
+
+anim = FuncAnimation(fig, animate, interval=10, frames=len(offsets)-1, repeat = False)
+
+plt.draw()
+plt.show()
+
